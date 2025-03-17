81a4b4c3a2d2985cc8fd236af468a6b31bedc152
restart126 <2476278368@qq.com>
2025-03-17 20:59:33 +0800

    Initial commit

---
+++ include/test.h
@@ -0,0 +1,10 @@
+//
+// Created by 24762 on 2025/3/11.
+//
+
+#ifndef CODE_COMPETITION_TEST_H
+#define CODE_COMPETITION_TEST_H
+
+int Acc(int a,int b);
+
+#endif //CODE_COMPETITION_TEST_H
---
+++ realize/test.cpp
@@ -0,0 +1,8 @@
+//
+// Created by 24762 on 2025/3/11.
+//
+#include "test.h"
+
+int Acc(int a,int b){
+    return  a+b;
+}
